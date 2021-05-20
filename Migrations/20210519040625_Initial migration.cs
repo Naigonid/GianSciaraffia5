cc699f7447c5686c017cadb9491ebeeaa2f0c6fd
Naigonid <gdsciaraffia@gmail.com>
2021-05-20 14:05:05 -0400

    Initial commit

---
+++ Migrations/20210519040625_Initial migration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GianSciaraffia.Migrations
+{
+    public partial class Initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "nombre",
+                table: "usuarioC",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "nombre",
+                table: "usuarioC");
+        }
+    }
+}
